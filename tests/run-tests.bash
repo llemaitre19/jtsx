c81cb7addc8e2f8d867d1da8ed40c27ef95c2125
Loïc Lemaître <loic.lemaitre@gmail.com>
2023-11-24 12:17:29 +0100

    Run package lint before tests suite.

--- tests/run-tests.bash
+++ tests/run-tests.bash
@@ -11,8 +11,24 @@ rm -rf $init_dir
 
 # Run tests on each Emacs versions
 for version in ${emacs_versions[@]}; do
-    if ! nix run --accept-flake-config github:purcell/nix-emacs-ci#$version -- -Q \
-         --init-directory=$init_dir --batch -l ert -l ../jtsx.el -l jtsx-tests.el \
+    emacs_nix="nix run --accept-flake-config github:purcell/nix-emacs-ci#$version --"
+    printf "RUN PACKAGE-LINT ON '%s'.\n" $version
+    if ! $emacs_nix --batch \
+         --eval "(progn \
+                (require 'package) \
+                (push '(\"melpa\" . \"https://melpa.org/packages/\") package-archives) \
+                (package-initialize) \
+                (unless package-archive-contents \
+                (package-refresh-contents)) \
+                (unless (package-installed-p 'package-lint) \
+                        (package-install 'package-lint)))" \
+         --eval "(require 'package-lint)" \
+         -f package-lint-batch-and-exit ../jtsx.el; then
+       printf "PACKAGE LINT FAILURE ON '%s'.\n" $version
+       exit
+    fi
+    printf "RUN TESTS ON '%s'.\n" $version
+    if ! $emacs_nix --init-directory=$init_dir --batch -l ert -l ../jtsx.el -l jtsx-tests.el \
          --eval "(jtsx-install-treesit-language 'javascript)" \
          --eval "(jtsx-install-treesit-language 'tsx)" \
          -f ert-run-tests-batch-and-exit; then
